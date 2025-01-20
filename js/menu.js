4cbb0ac50429de51d9f27279914e6fd0a25fe00f
Omar <omarboulakchour@gmail.com>
2025-01-20 16:14:24 +0100

    creacion del menu dinamico para movil

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,13 @@
-const menu = document.getElementById('nav_open'); // El menú móvil
-const open = document.getElementById('menu_movile'); // Botón para abrir/cerrar
-const img = document.getElementById('menu'); // Imagen del botón
+const menu = document.getElementById('nav_open'); 
+const open = document.getElementById('menu_movile'); 
+const img = document.getElementById('menu'); 
 
 open.addEventListener('click', () => {
-    // Alterna la clase de visibilidad del menú
     menu.classList.toggle('nav_visible');
 
-    // Verifica si el menú es visible para cambiar la imagen
     if (menu.classList.contains('nav_visible')) {
-        img.setAttribute('src', './img/close.png'); // Imagen para cerrar
+        img.setAttribute('src', './img/close.png'); 
     } else {
-        img.setAttribute('src', './img/Menu.png'); // Imagen para abrir
+        img.setAttribute('src', './img/Menu.png'); 
     }
 });
